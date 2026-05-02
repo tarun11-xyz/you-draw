3f2b7179e3278b287108e8e4d8d595da59698ea4
Tarun Gaur <tarun11.xyz@gmail.com>
2026-05-02 13:05:12 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+try {
+  const _k = 'T3JpZ2luYWwgRGV2ZWxvcGVyOiBUYXJ1biAodGFydW4xMS54eXpAZ21haWwuY29tKSB8IEFwcDogWW91RHJhdw==';
+  Object.defineProperty(window, '_yd_core_identity', {
+    get: () => atob(_k),
+    enumerable: false,
+    configurable: false
+  });
+  console.debug('%c[Core] init', 'color:transparent;font-size:1px;padding:0;');
+} catch(e) {}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
